99453b793ccf53844066e0a524b0d467e0ddbab7
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2025-06-29 15:36:36 -0500

    Improved efficiency of reflection

--- PermaFlukes.cs
+++ PermaFlukes.cs
@@ -1,17 +1,22 @@
 ﻿using Modding;
 using System.Reflection;
 using UnityEngine;
+
 namespace PermaFlukes {
     public class PermaFlukes: Mod {
         new public string GetName() => "PermaFlukes";
-        public override string GetVersion() => "1.0.0.0";
+        public override string GetVersion() => "1.0.0.1";
+
         public override void Initialize() {
             On.SpellFluke.Burst += dontBurst;
             On.SpellFluke.OnEnable += addBurstComp;
+            TransitionBurster.staticDefine();
         }
+
         private void dontBurst(On.SpellFluke.orig_Burst orig, SpellFluke self) {
             return;
         }
+
         private void addBurstComp(On.SpellFluke.orig_OnEnable orig, SpellFluke self) {
             orig(self);
             if(self.gameObject.TryGetComponent(out TransitionBurster tb))
@@ -22,19 +27,26 @@ namespace PermaFlukes {
     }
     public class TransitionBurster: MonoBehaviour {
         public string sceneName = "";
-        private FieldInfo meshRenderer, body;
+        private MeshRenderer mr;
+        private Rigidbody2D bd;
+        private static FieldInfo meshRenderer, body;
+
         public TransitionBurster() {
             sceneName = GameManager.instance.sceneName;
-            meshRenderer = typeof(SpellFluke).GetField("meshRenderer",
-            BindingFlags.NonPublic | BindingFlags.Instance);
-            body = typeof(SpellFluke).GetField("body",
-            BindingFlags.NonPublic | BindingFlags.Instance);
         }
+
+        public static void staticDefine() {
+            meshRenderer = typeof(SpellFluke).GetField("meshRenderer", BindingFlags.NonPublic | BindingFlags.Instance);
+            body = typeof(SpellFluke).GetField("body", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
         private void Update() {
             if(GameManager.instance.sceneName != sceneName) {
                 SpellFluke self = gameObject.GetComponent<SpellFluke>();
-                MeshRenderer mr = meshRenderer.GetValue(self) as MeshRenderer;
-                Rigidbody2D bd = body.GetValue(self) as Rigidbody2D;
+                if(mr == null)
+                    mr = meshRenderer.GetValue(self) as MeshRenderer;
+                if(bd == null)
+                    bd = body.GetValue(self) as Rigidbody2D;
                 if(mr)
                     mr.enabled = false;
                 if(bd) {
